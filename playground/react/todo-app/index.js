65889ffcfa938b51076459a40f9e72c6b92699b3
Arun <m.arunkumar@live.in>
2022-04-12 22:30:58 +0530

    Making it work on gh-pages

--- playground/react/todo-app/index.js
+++ playground/react/todo-app/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+function MyApp() {
+  return <h1>Hello, world!</h1>;
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<MyApp />);
